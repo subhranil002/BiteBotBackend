32c79eb6f74b992b4a0ee6bfa6a01eac628002f8
wasiqafnan <wansari964@yahoo.com>
2025-08-07 19:21:01 +0530

    minor bug fix in ApiError class

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,7 @@
+import { ApiResponse, ApiError } from "../utils/index.js";
 export const handleRegister = (req, res) => {
-    res.status(200).json()
+    res.status(200).json();
 };
 export const handleLogin = (req, res) => {
-    res.status(200).json()
+    res.status(200).json();
 };
