7cc172a78b09db5728e8a633cb1c0a9a2ac5f501
WasiqAfnan <wansari964@yahoo.com>
2025-10-11 14:59:48 +0530

    Fix minor issue

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -12,7 +12,7 @@ import {
 export const handleRegister = async (req, res, next) => {
     try {
         // get name, email and pw from body
-        const { email, password, profile_name, profile_cuisine, profile_dietryLabels } = req.body;
+        const { email, password, profile_name, profile_cuisine, profile_dietaryLabels } = req.body;
 
         // validate
         if (!(email && password && profile_name && profile_cuisine)) {
@@ -47,7 +47,7 @@ export const handleRegister = async (req, res, next) => {
             cuisine: profile_cuisine,
         };
 
-        if(profile_dietryLabels) dietryLabels = profile_dietryLabels;
+        if(profile_dietaryLabels) dietaryLabels = profile_dietaryLabels;
 
         // Create new user object
         const newUser = new User({
